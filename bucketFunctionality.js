567f47628d5b44b1b73aa3667d6ffaee357a3d7e
FroggyRocky <43971330+FroggyRocky@users.noreply.github.com>
2021-10-26 14:00:09 -0700

    Refactoring

--- bucketFunctionality.js
+++ bucketFunctionality.js
@@ -1,34 +1,43 @@
 
+
+///RENDERING///
 if(localStorage.getItem('cartData')) {
-////RENDERING/////
     const cartOut = document.querySelector(".cart-bucket")
     const cartData = JSON.parse(localStorage.getItem('cartData'))
     const shopCart1 = new shopCart(cartData)
-    cartOut.innerHTML = '';
-    cartOut.append(shopCart1.render())
+    refresh(cartOut, shopCart1)
 
-////FUNCTIONALITY/////
+///FUNCTIONALITY///
+
 cartOut.addEventListener("click", function(){
-    const {classList, dataset} = event.target
+const {classList, dataset} = event.target
 
-   if(classList.contains('delete')) {
+   if(classList.contains('delete')) { ///Delete///
        console.log("delete");
        shopCart1.deleteOne(dataset['articul'])
-       cartOut.innerHTML = ""
-       cartOut.append(shopCart1.render());
-       localStorage.setItem('cartData', JSON.stringify(shopCart1.items))
+       refresh(cartOut, shopCart1, 'cartData')
    } 
+   ///MINUS///
    else if (classList.contains("minus")) {
        shopCart1.minusOne(dataset['articul'])
-       cartOut.innerHTML = '';
-       cartOut.append(shopCart1.render())
-       localStorage.setItem('cartData', JSON.stringify(shopCart1.items))
+       refresh(cartOut, shopCart1, 'cartData')
    }
+   ///PLUS///
    else if (classList.contains('plus')) {
-       shopCart1.plusOne(dataset['articul'])
-       cartOut.innerHTML = '';
-       cartOut.append(shopCart1.render())
-       localStorage.setItem('cartData', JSON.stringify(shopCart1.items))
+      shopCart1.plusOne(dataset['articul'])
+      refresh (cartOut, shopCart1, 'cartData')
+
    }
 })
 }
+
+
+
+///RERENDER CART AND REFRESH DATA///
+function refresh(renderPlace, cartObj, cartLocalData) {
+    renderPlace.innerHTML = ""
+    renderPlace.append(cartObj.render());
+    localStorage.setItem(cartLocalData, JSON.stringify(cartObj.items))
+}
+
+
